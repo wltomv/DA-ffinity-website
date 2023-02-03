b77a54caaf89bb173b002825f251e42acb8067fc
Walter Mach <owaldovelasz@gmail.com>
2023-02-03 11:53:29 -0600

    refactor: new properties in the modal component and modification of styles

--- src/containers/Modal/Modal.jsx
+++ src/containers/Modal/Modal.jsx
@@ -3,12 +3,12 @@ import { AiFillCloseCircle } from 'react-icons/ai';
 
 import './Modal.scss';
 
-function Modal({ children, isOpen, closeModal }) {
+function Modal({ children, isOpen, closeModal, title }) {
     return (
         <article className={`modal ${isOpen && 'is-open'}`}>
             <div className="modal-container">
                 <div className="modal-header">
-                    <h3>titulo</h3>
+                    <h3>{title}</h3>
                 </div>
                 <button
                     type="button"
@@ -27,6 +27,9 @@ Modal.propTypes = {
     children: PropTypes.node.isRequired,
     isOpen: PropTypes.bool.isRequired,
     closeModal: PropTypes.func.isRequired,
+    title: PropTypes.string,
 };
+
+Modal.defaultProps = { title: '' };
 
 export default Modal;
