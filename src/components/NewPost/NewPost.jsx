9262a447c9c9a4f0597de868bb993590f66a577c
Walter Mach <owaldovelasz@gmail.com>
2023-02-02 22:09:45 -0600

    refactor(New Post): replace user image with a separate component

--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { FaImages } from 'react-icons/fa';
 import images from '../../constants/images';
+import Avatar from '../Avatar/Avatar';
 import './NewPost.scss';
 
 function NewPost() {
     return (
         <div className="new__post">
-            <img src={images.userImg} alt="user" />
+            <Avatar imgSrc={images.userImg} />
             <div className="new__post__container">
                 <input type="text" placeholder="Nueva publicación" />
                 <div className="new__post__container-media">
