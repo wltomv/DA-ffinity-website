5e9e24f645382da7dfa7093d8112824418d40965
Walter Mach <owaldovelasz@gmail.com>
2023-02-06 09:37:16 -0600

    fix: undefined reference to address account variable

--- src/hooks/useTrimAddress.js
+++ src/hooks/useTrimAddress.js
@@ -1,8 +1,8 @@
-const { useMemo } = require('react');
+import { useMemo } from 'react';
 
 const useTrimAddress = (address) => {
     const hide = useMemo(
-        () => `${address.substr(0, 4)}...${address.substr(-4)}`,
+        () => `${address?.substr(0, 6)}...${address?.substr(-2)}`,
         [address]
     );
     return hide;
